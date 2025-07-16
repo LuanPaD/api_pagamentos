cce759f9176dda2184c74be829afad09d6e13dd6
Luan <111983442+LuanPaD@users.noreply.github.com>
2025-07-16 12:27:24 -0300

    feat: Spring Boot project setup

---
+++ src/main/java/com/luanmiranda/api_pagamento/ApiPagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.luanmiranda.api_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPagamentoApplication.class, args);
+	}
+
+}
